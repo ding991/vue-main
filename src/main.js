43af421fdd0a1d94dfe3901b999340aa6f5cb66f
丁盾 <dun_ding@fpi-inc.com>
2021-12-03 15:31:54 +0800

    feat: init project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'ant-design-vue/dist/antd.css'
+import ProLayout, { PageHeaderWrapper } from './components/ProLayout'
+import './core/lazy_use'
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
+// 组件全局注册
+Vue.component('pro-layout', ProLayout)
+Vue.component('page-header-wrapper', PageHeaderWrapper)
 
 new Vue({
-  render: h => h(App),
+    router,
+    store,
+    render: h => h(App),
 }).$mount('#app')
